df289af3492a29e72e16cd7ede357bf0888cf089
DBRETAS <111098380+DBRETAS@users.noreply.github.com>
2023-10-25 18:22:00 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function home(){
     return <h1>Todo dia mais perto do meu sonho, 
-            eu te amo e obrigado por me apoiar S2</h1>
+            eu te amo e obrigado por me apoiar ❤️❤️</h1>
 }
 
 export default home;
