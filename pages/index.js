dfeece27c1df7a3ce6bad545b66f7a73c6a55282
DBRETAS <111098380+DBRETAS@users.noreply.github.com>
2023-11-17 16:43:18 +0000

    N

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 function home() {
   return (
     <h1>
-      {" "}
-      Todo dia mais perto do meu sonho, eu te amo e obrigado por me apoiar S2{" "}
+      Todo dia mais perto do meu sonho, eu te amo e obrigado por me apoiar S2
     </h1>
   );
 }
